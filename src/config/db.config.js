483716a978e858dac888f0c6deabcd217c2a66f2
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-06-24 23:44:07 -0300

    att

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -13,3 +13,5 @@ connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
     if (err) throw err;
     console.log('Database connection successful!');
 });
+
+module.exports = connection;
